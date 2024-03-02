e4a2f34e263d726c0c164795d37e857806fee097
Ben Naylor <nayben@gmail.com>
2024-03-02 10:59:52 +0100

    Rely on age check for outputting dead/alive
    No need to store any extra memory, and float comparisons aren't too
    expensive

--- src/yak.rs
+++ src/yak.rs
@@ -4,11 +4,6 @@ use std::fmt::{self, Display, Formatter};
 const DAYS_IN_YAK_YEAR: f64 = 100.0;
 const MAX_YAK_AGE: f64 = 10.0;
 
-// The default value for the `alive` field
-fn default_alive() -> bool {
-    true
-}
-
 #[derive(Deserialize, Debug)]
 #[allow(never_read)]
 pub struct Yak {
@@ -17,16 +12,14 @@ pub struct Yak {
     age: f64,
     /// The age of the yak when it was last shaved
     #[serde(skip_deserializing)]
-    age_last_shaved: f32,
-    #[serde(skip_deserializing, default = "default_alive")]
-    alive: bool,
+    age_last_shaved: f64,
 }
 
 impl Display for Yak {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{} {} years old", self.name, self.age)?;
 
-        if !self.alive {
+        if self.age >= MAX_YAK_AGE {
             write!(f, " (dead)")?;
         }
 
@@ -36,14 +29,10 @@ impl Display for Yak {
 
 impl Yak {
     pub fn step_days(&mut self, days: u32) {
-        if !self.alive {
+        if self.age >= MAX_YAK_AGE {
             return;
         }
 
         self.age += f64::from(days) / DAYS_IN_YAK_YEAR;
-
-        if self.age > MAX_YAK_AGE {
-            self.alive = false;
-        }
     }
 }
