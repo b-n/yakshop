fe12fdc36f6a95e2b750fba9c9a4316ea81de0d4
Ben Naylor <nayben@gmail.com>
2024-03-02 09:40:58 +0100

    Setup commandline parsing and xml deserializing
    Notes:
    
    - Starting with MVP
    - Days arg does nothing (for now)
    - Sex is part of the file, however it is never displayed or output so it
      is being ignored (for now)

--- README.md
+++ README.md
@@ -5,15 +5,28 @@ A small app for calculating yak outputs for a webshop
 ## Running
 
 ```console
-you@yakshop > ./yakshop
-Hello World!
+you@yakshop > ./yakshop --help
+A simple yak shop simulator
+
+Usage: yakshop <HERD>
+
+Arguments:
+  <HERD>  The location to the herd.xml file to use as a data source
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
 ```
+
+Run development mode with a herd.xml file:
+
+`cargo run -- ./examples/herd.xml`
 
 ## Developing
 
 Run Yakshop directly with cargo:
 
-`cargo run`
+`cargo run -- ./examples/herd.xml`
 
 Alterantively, a development release can be built and run:
 
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,115 @@
 #![warn(unused_qualifications)]
 #![warn(variant_size_difference)]
 
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use serde::Deserialize;
+use std::error::Error;
+use std::fmt::{self, Display, Formatter};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// The location to the herd.xml file to use as a data source.
+    #[arg(required = true)]
+    pub herd: PathBuf,
+
+    /// The number of days to run the simulation for.
+    #[arg(required = true)]
+    pub days: usize,
+}
+
+#[derive(Debug)]
+pub enum YakShopError {
+    ConfigFileNotFound(PathBuf),
+}
+
+impl Display for YakShopError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            YakShopError::ConfigFileNotFound(path) => {
+                write!(f, "Config file {} not found", path.to_string_lossy())
+            }
+        }
+    }
+}
+
+impl Error for YakShopError {}
+
+#[derive(Deserialize, Debug)]
+enum YakSex {
+    #[serde(rename = "f")]
+    Female,
+    #[serde(rename = "m")]
+    Male,
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(never_read)]
+struct Yak {
+    name: String,
+    age: f32,
+}
+
+impl Display for Yak {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} years old", self.name, self.age,)
+    }
+}
+
+#[derive(Default, Deserialize, Debug)]
+pub struct Shop {
+    #[serde(rename = "$value")]
+    yaks: Vec<Yak>,
+    #[serde(skip_deserializing)]
+    milk: f64,
+    #[serde(skip_deserializing)]
+    skins: usize,
+}
+
+impl Display for Shop {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            r#"In Stock:
+    {} liters of milk
+    {} skins of wool
+Herd:"#,
+            self.milk, self.skins
+        )?;
+
+        for yak in &self.yaks {
+            write!(f, "\n    {yak}")?;
+        }
+
+        Ok(())
+    }
+}
+
+impl TryFrom<&PathBuf> for Shop {
+    type Error = YakShopError;
+
+    fn try_from(path: &PathBuf) -> Result<Self, Self::Error> {
+        let herd_config = path.as_path();
+        if !herd_config.exists() {
+            return Err(YakShopError::ConfigFileNotFound(path.clone()));
+        }
+
+        let herd_xml =
+            std::fs::read_to_string(herd_config).expect("Could not read herd.xml file to string");
+
+        let shop: Shop = serde_xml_rs::from_str(&herd_xml).expect("Could not parse herd.xml file");
+
+        Ok(shop)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let shop = Shop::try_from(&args.herd)?;
+
+    println!("Day: {}\n\n{shop}", args.days);
+
+    Ok(())
 }
